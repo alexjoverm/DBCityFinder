2d413fcfaaaa3848e2e363a4befa67cac623209b
Alejandro Jover Morales <ajover.incoming@fh-salzburg.ac.at>
2015-06-09 19:13:49 +0200

    Added map, population, countries json and fake data

--- app/scripts/controllers/homeCtrl.js
+++ app/scripts/controllers/homeCtrl.js
@@ -4,7 +4,9 @@ angular.module('mmtFinalExamApp')
   .controller('HomeCtrl', function ($scope, DataSvc) {
 
     $scope.map = DataSvc.map;
-    $scope.markers = [];
+    $scope.markers = DataSvc.markers;
+    $scope.population = DataSvc.population;
+    $scope.countries = DataSvc.countries;
 
 
     /****** Functions ******/
@@ -19,7 +21,10 @@ angular.module('mmtFinalExamApp')
 
     $scope.$on('DataSvc:dataLoaded', function(){
       $scope.markers = DataSvc.markers;
-      $scope.results = DataSvc.results;
+    });
+
+    $scope.$on('DataSvc:loadedCountries', function(){
+      $scope.countries = DataSvc.countries;
     });
 
 
--- app/scripts/services/dataSvc.js
+++ app/scripts/services/dataSvc.js
@@ -1,9 +1,49 @@
 'use strict';
 
 angular.module('mmtFinalExamApp')
-  .factory('DataSvc', function ($rootScope) {
+  .factory('DataSvc', function ($rootScope, $http) {
 
     /*********  PRIVATE API  *********/
+
+    // Static data
+
+    var _population = [
+      {
+        text: '0 - 1,000',
+        min: 0,
+        max: 1000
+      },
+      {
+        text: '1,000 - 10,000',
+        min: 1000,
+        max: 10000
+      },
+      {
+        text: '10,000 - 100,000',
+        min: 10000,
+        max: 100000
+      },
+      {
+        text: '100,000 - 1,000,000',
+        min: 100000,
+        max: 1000000
+      },
+      {
+        text: '> 1,000,000',
+        min: 1000000,
+        max: 10000000000
+      }
+    ];
+
+    $http.get('scripts/static/countries.json').success(function(data, status, headers, config) {
+      self.countries = data;
+      $rootScope.$broadcast('DataSvc:loadedCountries');
+    }).error(function(data, status, headers, config) {
+      console.log(data);
+    });
+
+
+    // Functions
 
     var createMarker = function(lat, lng, title){
       return {
@@ -18,19 +58,26 @@ angular.module('mmtFinalExamApp')
 
 
 
+
+
+
+
     /***********  PUBLIC API  ***********/
 
     var self = {
+      population: _population,
+      countries: [],
       results: [],
       markers: [],
       map: {
         center: {latitude: 47.76, longitude: 13.07 },
-        zoom: 10
+        zoom: 9
       },
 
       InsertData: function(data){
 
         // FAKE!!!!
+        self.markers = [];
         self.markers.push( createMarker(47.716143, 13.090606, 'Puch') );
         self.markers.push( createMarker(47.813840, 13.053755, 'Salzburg') );
 
